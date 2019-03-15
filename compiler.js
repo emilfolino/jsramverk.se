73e708bf9998170efab7fb317c8acf05281623cd
Emil Folino <emilfolino@gmail.com>
2019-03-15 16:02:39 +0100

    Fixed compiler for assignments

--- compiler.js
+++ compiler.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 
+const markdown = require("markdown").markdown;
+
 const includes = "./includes/";
 const content = "./content/";
 const output = "./output/";
@@ -30,26 +32,66 @@ const createIndex = function (header) {
 }
 
 const createAssignments = function (header) {
-    fs.readFile(`${includes}index.html`, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err.message);
-        }
+    fs.readdir(content, (err, files) => {
+        files.forEach(file => {
+            fs.readFile(`${content}${file}`, 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err.message);
+                }
 
-        let outputContent = header + data;
+                let parsed = markdown.parse(data);
 
-        addFooterAndWriteToFile(outputContent, "index.html");
+                let h1 = parsed.filter((element) => {
+                    return Array.isArray(element) &&
+                        element[0] === 'header' &&
+                        element[1].level === 1;
+                })[0][2];
+
+                let headers = parsed.filter((element) => {
+                    return Array.isArray(element) &&
+                        element[0] === 'header' &&
+                        element[1].level > 1;
+                });
+
+                let breadcrump = createBreadCrumbs(h1);
+                let toc = createTOC(headers);
+                let article = createArticle(markdown.toHTML(data));
+                let result = header + breadcrump + toc + article;
+                let filename = file.replace("md", "html");
+
+                addFooterAndWriteToFile(result, filename);
+            });
+        });
     });
+};
+
+const createBreadCrumbs = function (current) {
+    return `<p class='breadcrump'><a href='/'>jsramverk.me</a> / ${current}</p>`;
+};
+
+const createTOC = function (headers) {
+    let output = "<ul class='toc'>";
+    headers.forEach((header) => {
+        output += `<li><a href='#'>${header[2]}</a></li>`;
+    });
+    output += "</ul>";
+
+    return output;
+};
+
+const createArticle = function (content) {
+    return `<article class='week'>${content}</article>`;
 }
 
-const addFooterAndWriteToFile = function (content, filename) {
+const addFooterAndWriteToFile = function (outputContent, filename) {
     fs.readFile(`${includes}footer.html`, 'utf8', (err, data) => {
         if (err) {
             console.error(err.message);
         }
 
-        content += data;
+        outputContent += data;
 
-        fs.writeFile(`${output}${filename}`, content, (err) => {
+        fs.writeFile(`${output}${filename}`, outputContent, (err) => {
             if (err) {
                 console.error(err.message);
             }
@@ -60,3 +102,4 @@ const addFooterAndWriteToFile = function (content, filename) {
 };
 
 createHeader(createIndex);
+createHeader(createAssignments);
---
+++ content/week1.md
@@ -0,0 +1,9 @@
+# Frontend
+
+Vi utvärderar frontend ramverk och skapar en me-applikation i det JavaScript ramverk du valde.
+
+## läsa
+
+## titta
+
+## kravspec
