6a48d5d6dc1ea4e07d681dd48697f5cd45b6b86a
Emil Folino <emilfolino@gmail.com>
2019-04-03 12:51:21 +0200

    Changed build flow with webpack instead of manual

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const compiler = require("./compiler.js");
 compiler.createHeader(compiler.createIndex);
 compiler.createHeader(
     compiler.createAssignments,
-    ["atom-one-dark.css"],
-    [
-        "scroll.min.js",
-        "highlight.pack.js"
-    ]
+    [],
+    ["bundle.js"]
 );
---
+++ client.js
@@ -0,0 +1,16 @@
+import "./client-js/scroll.js";
+
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+import html from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+import bash from 'highlight.js/lib/languages/bash';
+
+import 'highlight.js/styles/monokai-sublime.css';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('html', html);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('bash', bash);
+
+hljs.initHighlightingOnLoad();
