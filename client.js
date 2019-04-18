f9f95c6514b3e6dbe12b3d3e39d866c58038577e
Emil Folino <emilfolino@gmail.com>
2019-04-18 07:58:10 +0200

    Made turbolinks and highlightjs work together

--- client.js
+++ client.js
@@ -5,6 +5,7 @@ import javascript from 'highlight.js/lib/languages/javascript';
 import html from 'highlight.js/lib/languages/xml';
 import css from 'highlight.js/lib/languages/css';
 import bash from 'highlight.js/lib/languages/bash';
+import json from 'highlight.js/lib/languages/json';
 
 import 'highlight.js/styles/monokai-sublime.css';
 
@@ -14,6 +15,12 @@ hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('html', html);
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('json', json);
 
-hljs.initHighlightingOnLoad();
 Turbolinks.start();
+
+document.addEventListener("turbolinks:load", function() {
+    document.querySelectorAll('pre code').forEach((block) => {
+        hljs.highlightBlock(block);
+    });
+});
