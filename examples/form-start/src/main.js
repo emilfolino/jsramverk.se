daae66b312db5e25a818ddece0f970ff71f9e867
Emil Folino <emilfolino@gmail.com>
2019-09-10 12:04:50 +0200

    After lecture week 2

---
+++ examples/form-start/src/main.js
@@ -0,0 +1,9 @@
+import ingredients from "./ingredients.js";
+
+(function IIFE() {
+    'use strict';
+
+    console.log(ingredients.getSortedIngredients());
+
+    console.log("hej di li hopp");
+})();
--- examples/form/src/main.js
+++ examples/form/src/main.js
@@ -1,9 +1,76 @@
 import ingredients from "./ingredients.js";
 
+
 (function IIFE() {
     'use strict';
+    let selectedIngredients = [];
 
-    console.log(ingredients.getSortedIngredients());
 
-    console.log("hej di li hopp");
+
+    let dropdown = document.getElementById("ingredient");
+    const allIngredients = ingredients.getSortedIngredients();
+
+    let allOptionGroup = document.createElement("optgroup");
+    allOptionGroup.label = "Alla ingredienser";
+
+    let selectedOptionGroup = document.createElement("optgroup");
+    selectedOptionGroup.label = "Tidigare valda ingredienser";
+
+    allIngredients.forEach(function(ingredient) {
+        let optionElement = document.createElement("option");
+
+        optionElement.value = ingredient;
+        optionElement.textContent = ingredient;
+
+        allOptionGroup.appendChild(optionElement);
+    });
+
+    dropdown.appendChild(allOptionGroup);
+
+    dropdown.addEventListener("change", function changeEvent(event) {
+        fillSelected(event);
+    });
+
+    if (localStorage.getItem("selectedIngredients")) {
+        selectedIngredients = JSON.parse(
+            localStorage.getItem("selectedIngredients")
+        );
+
+        fillSelected();
+    }
+
+    function fillSelected (event=false) {
+        if (event) {
+            selectedIngredients.unshift(event.target.value);
+            if (selectedIngredients.length > 3) {
+                selectedIngredients.pop();
+            }
+        }
+
+        while (dropdown.firstChild) {
+            dropdown.removeChild(dropdown.firstChild);
+        }
+
+        while (selectedOptionGroup.firstChild) {
+            selectedOptionGroup.removeChild(selectedOptionGroup.firstChild);
+        }
+
+        selectedIngredients.forEach(function(ingredient) {
+            let optionElement = document.createElement("option");
+
+            optionElement.value = ingredient;
+            optionElement.textContent = ingredient;
+
+            selectedOptionGroup.appendChild(optionElement);
+        });
+
+        dropdown.appendChild(selectedOptionGroup);
+
+        dropdown.appendChild(allOptionGroup);
+
+        localStorage.setItem(
+            "selectedIngredients",
+            JSON.stringify(selectedIngredients)
+        );
+    }
 })();
